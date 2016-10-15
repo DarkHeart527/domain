24669774741a97e6d51fb0466df1a5c4408f776f
DarkHeart527 <darkheart527s@gmail.com>
2016-10-14 18:52:40 -0700

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 var pageNum = 1;
+var sec1 = document.getElementById('section1');
 
 function checkNav() {
  if (pageNum == 1) {
@@ -14,5 +15,5 @@ function pageEmpty() {
 }
 
 function page1() {
-
+ sec1.innerHtml = "Hey";
 }
