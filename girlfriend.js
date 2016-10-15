bbc63f961847977d586dd8a564f1cfdb4b1c3cbc
DarkHeart527 <darkheart527s@gmail.com>
2016-10-15 09:31:29 -0700

    Update girlfriend.js

--- girlfriend.js
+++ girlfriend.js
@@ -2,5 +2,5 @@ setInterval( function() { movePic() }, 200);
 
 function movePic() {
  setTimeout( function() { document.getElementById('body').style = 'url("https://darkheart527.github.io/domain-storage/Heart.png") repeat fixed';}, 100)
- setTimeout( function() { document.getElementById('body').style = 'url("https://darkheart527.github.io/domain-storage/Heart2.png") repeat fixed';}, 200)
+ //setTimeout( function() { document.getElementById('body').style = 'url("https://darkheart527.github.io/domain-storage/Heart2.png") repeat fixed';}, 200)
 }
