fdea0c90a56a2271985a204d0f8f43be5fd8b4c0
DarkHeart527 <darkheart527s@gmail.com>
2016-10-15 03:43:28 -0700

    Update home.js

--- home.js
+++ home.js
@@ -14,5 +14,8 @@ function pageEmpty() {
 }
 
 function page1() {
- document.getElementById('navHeader').innerHTML = '<a href="#" onclick="" class="page"><<</a><a class="linkSelection" href="http://girlfriend.codark.com">Girlfriend&#39;s Page</a><a class="linkSelection" href="http://projects.codark.com">Projects</a><a class="linkSelection" href="http://deathClicker.codark.com">Death Clicker</a><a href="#" class="page" onclick="addOne()">>></a>';
+ document.getElementById('navHeader').innerHTML =
+  '<center>' +
+  '<a href="#" onclick="" class="page"><<</a><a class="linkSelection" href="http://girlfriend.codark.com">Girlfriend&#39;s Page</a><a class="linkSelection" href="http://projects.codark.com">Projects</a><a class="linkSelection" href="http://deathClicker.codark.com">Death Clicker</a><a href="#" class="page" onclick="addOne()">>></a>' +
+  '</center>';
 }
