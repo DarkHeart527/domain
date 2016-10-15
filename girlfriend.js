ff22e3b1f5a476e3d9b81721d2d9385546cd5b61
DarkHeart527 <darkheart527s@gmail.com>
2016-10-15 09:32:29 -0700

    Update girlfriend.js

--- girlfriend.js
+++ girlfriend.js
@@ -1,4 +1,4 @@
-setInterval( function() { movePic() }, 200);
+setInterval( function() { movePic() }, 2000);
 
 function movePic() {
  setTimeout( function() { document.getElementById('body').style = 'url("https://darkheart527.github.io/domain-storage/Heart.png") repeat fixed';}, 100)
