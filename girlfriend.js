95f3e49c79658f571edebcbc54b53e3f9b547575
DarkHeart527 <darkheart527s@gmail.com>
2016-10-28 18:35:30 -0700

    Update girlfriend.js

--- girlfriend.js
+++ girlfriend.js
@@ -8,8 +8,15 @@ function movePic() {
 function checkPerms() {
  if (globalCode == 2639301) {
   document.getElementById('loadVideo').src = "https://darkheart527.github.io/domain-storage/XiaoYing - James.mp4";
+  document.getElementById('gCArea').style.visibility = 'hidden';
+  alert("Successful Login - Page Ex's Page Logged Into")
+  localStorage.globalCode001 = globalCode;
  } else {
-  document.getElementById('gCArea').style.visibility = true;
+  document.getElementById('gCArea').style.visibility = 'hidden';
   alert('Your Gc is incorrect');
  }
+}
+
+function loadGlobal() {
+ document.getElementById('gCArea').value = Number(localStorage.globalCode001)
 }
