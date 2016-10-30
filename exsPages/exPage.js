5b013f2cf32898c8be0d36a619e7daacd583b98f
DarkHeart527 <darkheart527s@gmail.com>
2016-10-29 23:28:15 -0700

    Update exPage.js

--- exsPages/exPage.js
+++ exsPages/exPage.js
@@ -1,10 +1,3 @@
-setInterval( function() { movePic() }, 200);
-
-function movePic() {
- setTimeout( function() { document.getElementById('body').style = "background: url('https://darkheart527.github.io/domain-storage/Heart.png') repeat fixed"}, 100);
- setTimeout( function() { document.getElementById('body').style = "background: url('https://darkheart527.github.io/domain-storage/Heart2.png') repeat fixed"}, 200);
-}
-
 function checkPerms() {
  if (globalCode == 2639301) {
   document.getElementById('loadVideo').src = "https://darkheart527.github.io/domain-storage/XiaoYing - James.mp4";
