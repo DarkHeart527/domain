0fe9d4d9bc30615a86e89ce68123773387b3f68b
DarkHeart527 <darkheart527s@gmail.com>
2016-11-03 14:09:38 -0700

    Update themeChanger.js

--- musicPlayer/themeChanger.js
+++ musicPlayer/themeChanger.js
@@ -1,3 +1,11 @@
 function themeSchool() {
+ document.getElementById('stylingLink').href = "musicplayerSchool.css";
+}
 
+function themeOriginal() {
+ document.getElementById('stylingLink').href = "musicplayerSchool.css";
+}
+
+function themeNoStyle() {
+ document.getElementById('stylingLink').href = "";
 }
