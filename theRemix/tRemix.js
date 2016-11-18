eaa095702819efc41e409c93056bd25c7e8ab9ce
DarkHeart527 <darkheart527s@gmail.com>
2016-11-18 14:31:00 -0800

    Update tRemix.js

--- theRemix/tRemix.js
+++ theRemix/tRemix.js
@@ -1,17 +1,25 @@
 var trackNumber = 0;
+var savedNumber = 0;
 
 function nextTrack() {
  trackNumber = Number(trackNumber + 1);
+ savedNumber = trackNumber;
  updateStuff();
 }
 
 function previousTrack() {
  trackNumber = Number(trackNumber - 1);
+ savedNumber = trackNumber;
  updateStuff();
 }
 
 function checkMusic() {
- 
+ if (trackNumber == 0) {
+  document.getElementById('').src = '';
+ } else {
+  trackNumber = savedNumber;
+  updateStuff();
+ }
 }
 
 function updateStuff() {
