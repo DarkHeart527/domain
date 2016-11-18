9879c96cc9b3749b4939f00172db506cd86d2f37
DarkHeart527 <darkheart527s@gmail.com>
2016-11-18 14:17:05 -0800

    Update tRemix.js

--- theRemix/tRemix.js
+++ theRemix/tRemix.js
@@ -1,1 +1,13 @@
+var trackNumber = 0;
 
+function nextTrack() {
+ trackNumber = trackNumber + 1;
+}
+
+function previousTrack() {
+ trackNumber = trackNumber - 1;
+}
+
+function checkMusic() {
+ 
+}
