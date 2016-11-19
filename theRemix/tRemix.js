c2cd08571573bf163366d2d9b4ced7c917e8344e
DarkHeart527 <darkheart527s@gmail.com>
2016-11-18 20:41:52 -0800

    Update tRemix.js

--- theRemix/tRemix.js
+++ theRemix/tRemix.js
@@ -1,5 +1,6 @@
 var trackNumber = 0;
 var savedNumber = 0;
+var link = 'music/';
 
 function nextTrack() {
  trackNumber = Number(trackNumber + 1);
@@ -19,11 +20,11 @@ function checkMusic() {
  if (trackNumber == 0) {
   document.getElementById('audioFile').src = '';
  } else if (trackNumber == 1) {
-  document.getElementById('audioFile').src = 'Right Now.mp3';
+  document.getElementById('audioFile').src = link + 'Right Now.mp3';
  } else if (trackNumber == 2) {
-  document.getElementById('audioFile').src = 'Alright.mp3';
+  document.getElementById('audioFile').src = link + 'Alright.mp3';
  } else if (trackNumber == 3) {
-  document.getElementById('audioFile').src = 'Like Me.mp3';
+  document.getElementById('audioFile').src = link + 'Like Me.mp3';
  } else {
   trackNumber = savedNumber;
   updateStuff();
