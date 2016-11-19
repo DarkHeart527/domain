9074415e8e5665ea1824872a7487979bef068b45
DarkHeart527 <darkheart527s@gmail.com>
2016-11-18 20:39:50 -0800

    Update tRemix.js

--- theRemix/tRemix.js
+++ theRemix/tRemix.js
@@ -17,7 +17,13 @@ function previousTrack() {
 
 function checkMusic() {
  if (trackNumber == 0) {
-  document.getElementById('').src = '';
+  document.getElementById('audioFile').src = '';
+ } else if (trackNumber == 1) {
+  document.getElementById('audioFile').src = 'Right Now.mp3';
+ } else if (trackNumber == 2) {
+  document.getElementById('audioFile').src = 'Alright.mp3';
+ } else if (trackNumber == 3) {
+  document.getElementById('audioFile').src = 'Like Me.mp3';
  } else {
   trackNumber = savedNumber;
   updateStuff();
