6a2be51dca4cffbe7bdd659592a0ad015a39b16f
DarkHeart527 <darkheart527s@gmail.com>
2016-12-01 14:37:03 -0800

    Update iF.js

--- iFrameVisitor/iF.js
+++ iFrameVisitor/iF.js
@@ -3,9 +3,9 @@ var webId2;
 
 function visit() {
   webId = document.getElementById('address').value;
-  webId = webId.split('https://');
-  webId.filter(Boolean);
-  webId2 = webId.split('http://');
+  //webId = webId.split('https://');
+  //webId.filter(Boolean);
+  //webId2 = webId.split('http://');
   document.getElementById('web').src = 'https://' + webId[0];
   document.getElementById('address').value = document.getElementById('web').src;
 }
