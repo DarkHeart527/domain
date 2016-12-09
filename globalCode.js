4f6b4d77d9de8c84c4b1f68bd3c394471096e11d
DarkHeart527 <darkheart527s@gmail.com>
2016-12-09 13:17:19 -0800

    Update globalCode.js

--- globalCode.js
+++ globalCode.js
@@ -1,2 +1,10 @@
+askForPerm();
+
 function askForPerm() {
- var globalCode = prompt("Please Type In Your Seven Digit Global Code To Login... Ex: 1234567","")
+ if (localStorage.New == 1) {
+  
+ } else {
+  var globalCode = prompt("Please Type In Your Seven Digit Global Code To Login... Ex: 1234567","");
+  if (
+ }
+}
