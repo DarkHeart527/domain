e470496052abcda623a1326d197d7017a5aebca5
DarkHeart527 <darkheart527s@gmail.com>
2017-01-29 18:32:21 -0800

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-alert('The Home Screen Is Currently Being Remade/nPlease Be Aware That Some Errors Can And Will Exist')
+alert('The Home Screen Is Currently Being Remade\nPlease Be Aware That Some Errors Can And Will Exist')
 
 var pageNum = 1;
 
@@ -6,15 +6,9 @@ function checkNav() {
  if (pageNum == 1) {
   page1();
  }
- else if (pageNum == 2) {
-  page2();
- } else if (pageNum == 3) {
-  page3();
- }
  else {
   pageEmpty();
  }
- document.getElementById('pageNum').innerHTML = pageNum;
 }
 
 function addOne() {
@@ -23,45 +17,63 @@ function addOne() {
 }
 
 function deleteOne() {
+if (document.getElementById('leftLink').disabled == true) {
+} else {
  pageNum = Number(pageNum - 1);
  checkNav();
+ }
 }
 
 function pageEmpty() {
- document.getElementById('navHeader').innerHTML = 
- '<a href="#" onclick="deleteOne()" class="page"><<</a>' +
- '<a href="#" class="page2" onclick="addOne()">>></a>';
+ page1Name = '';
+ page1Link = '';
+ page2Name = '';
+ page2Link = '';
+ page3Name = '';
+ page3Link = '';
+ page4Name = '';
+ page4Link = '';
+ page5Name = '';
+ page5Link = '';
+ page6Name = '';
+ page6Link = '';
+ changePages();
 }
 
 function page1() {
- document.getElementById('navHeader').innerHTML =
-  '<center>' +
-  '<a href="#" style="visibility: hidden;" onclick="" class="page"><<</a>' +
-  //'<a id="link1" class="linkSelection" href="http://colorclock.codark.com">Color Clock</a>' +
-  '<a id="link2" class="linkSelection" href="convert/start.html">Converter</a>' +
-  //'<a id="link3" class="linkSelection" href="projects/Unnamed.html">Unnamed</a>' +
-  //'<a id="link4" class="linkSelection" href="http://deathClicker.codark.com">Death Clicker</a>' +
-  //'<a id="link5" class="linkSelection" href="allowance/home.html">Allowance</a>' +
-  '<a href="#" class="page2" onclick="addOne();checkPermission()">>></a>' +
-  '</center>';
+ page1Name = 'Convert';
+ page1Link = 'convert/start.html';
+ page2Name = '';
+ page2Link = '';
+ page3Name = '';
+ page3Link = '';
+ page4Name = '';
+ page4Link = '';
+ page5Name = '';
+ page5Link = '';
+ page6Name = '';
+ page6Link = '';
+ changePages();
 }
 
-function page2() {
- document.getElementById('navHeader').innerHTML =
-  '<center>' +
-  '<a href="#" style="visibility: visible;" onclick="deleteOne();checkPermission()" class="page"><<</a>' +
-  //'<a id="link6" href="musicPlayer/Music%20Player.html" class="linkSelection">Music Player</a>' +
-  //'<a id="link7" href="iFrameVisitor/start.html" class="linkSelection">iFrame Visitor</a>' +
-  //'<a id="link8" href="theRemix/home.html" class="linkSelection">The Remix</a>' +
-  '<a href="#" class="page2" onclick="addOne();checkPermission()">>></a>' +
-  '</center>';
-}
-
-function page3() {
-  document.getElementById('navHeader').innerHTML =
-  '<center>' +
-  '<a href="#" style="visibility: visible;" onclick="deleteOne();checkPermission()" class="page"><<</a>' +
-  '<a href="" class="linkSelection"></a>' + //Code Area
-  '<a href="#" class="page2" onclick="addOne();checkPermission()">>></a>' +
-  '</center>';
+function changePages() {
+ if (pageNum == 1 | pageNum < 1) {
+  document.getElementById('leftLink').innerHTML = '';
+  document.getElementById('leftLink').disabled = true;
+ } else {
+  document.getElementById('leftLink').innerHTML = '(---';
+  document.getElementById('leftLink').disabled = false;
+ }
+ document.getElementById('link1').innerHTML = "<a id='link1A'>" + page1Name + "</a>";
+ document.getElementById('link1A').href = page1Link;
+ document.getElementById('link2').innerHTML = "<a id='link2A'>" + page2Name + "</a>";
+ document.getElementById('link2A').href = page2Link;
+ document.getElementById('link3').innerHTML = "<a id='link3A'>" + page3Name + "</a>";
+ document.getElementById('link3A').href = page3Link;
+ document.getElementById('link4').innerHTML = "<a id='link4A'>" + page4Name + "</a>";
+ document.getElementById('link4A').href = page4Link;
+ document.getElementById('link5').innerHTML = "<a id='link5A'>" + page5Name + "</a>";
+ document.getElementById('link5A').href = page5Link;
+ document.getElementById('link6').innerHTML = "<a id='link6A'>" + page6Name + "</a>";
+ document.getElementById('link6A').href = page6Link;
 }
