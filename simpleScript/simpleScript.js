489ca0312fa3a6da0c065cc7371fd70fc96e6dc5
James <DarkHeart527@users.noreply.github.com>
2017-04-27 12:36:08 -0700

    Update simpleScript.js

--- simpleScript/simpleScript.js
+++ simpleScript/simpleScript.js
@@ -2,46 +2,27 @@
 
 */
 
-var newElement = function(elementTag, idAddingTo, beforeAfter, html, idName, className, styling) {
-				  var info = {elementTag: elementTag,
-							  idTo: idAddingTo,
-							  beforeAfter: beforeAfter,
-							  html: html,
-							  idName: idName,
-							  className:className,
-							  styling: styling}
-							  
-				  elmtT = {openT: "<" + info.elementTag + " id='" + info.idName + "' class='" + info.className + "' style='" + info.styling + "'" + ">", closeT: "</" + info.elementTag + ">"}
-				  elem = document.getElementById(info.idTo);
-				  if (info.beforeAfter === 'after') {
-				   elem.innerHTML = elem.innerHTML + elmtT.openT + info.html + elmtT.closeT;
-				  } else if (info.beforeAfter === 'before') {
-				   elem.innerHTML = elmtT.openT + info.html + elmtT.closeT + elem.innerHTML;
-				  } else {
-				   console.error('Error: Syntax of "beforeAfter" is incorrect \nThis area is either AFTER or BEFORE\nValue = ' + info.beforeAfter);
-				  }
-				  if (info.className == undefined | info.className == null | info.className == 'none') {
-				   document.getElementById(info.idName).className = ''
-				  }
-				  if (info.styling == undefined | info.styling == null | info.styling == 'none') {
-				   document.getElementById(info.idName).style = ''
-				  }
-				  if (info.idName == undefined | info.idName == null | info.idName == 'none') {
-				   document.getElementById(info.idName).id = ''
-				  }
-				  console.log(info);
-				  console.log(elmtT);
-				 }
-var numberRandom = function(targetId, minNum, maxNum, useText, beforeAfter, iText) {
-				  if (useText == true) {
-				   if (beforeAfter == 'after') {
-					document.getElementById(targetId).innerHTML = Number(Math.floor(Math.random() * maxNum) + minNum) + iText;
-				   } else if (beforeAfter == 'before') {
-					document.getElementById(targetId).innerHTML = iText + Number(Math.floor(Math.random() * maxNum) + minNum);
-				   } else {
-				   console.error('Error: Syntax of "beforeAfter" is incorrect \nThis area is either AFTER or BEFORE\nValue = ' + beforeAfter);
-				   }
-				  } else {
-				   document.getElementById(targetId).innerHTML = Math.floor(Math.random() * maxNum) + minNum;
-				  }
-				 }
+var log = {
+ error: function(info) {
+ }
+}
+
+function getId(info) {
+ return document.getElementById(info);	
+}
+
+function getClasses(info) {
+ return document.getElementsByClassName(info);
+}
+
+function getFirstClass(info) {
+ return document.getElementsByClassName(info)[0];
+}
+
+function getElements(info) {
+ if (document.getElementsByTagName(info)) {
+ return document.getElementsByTagName(info);
+ } else {
+  log.error(	 
+ }
+}
