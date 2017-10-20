97209acfa11918188637054eed6105217b2a86e2
James <DarkHeart527@users.noreply.github.com>
2017-10-20 00:48:53 -0700

    Update script.js

--- beatWeb/scripts/script.js
+++ beatWeb/scripts/script.js
@@ -33,13 +33,24 @@ for (let i = 0; i < document.getElementsByTagName('img').length; i++) {
   }
 }
 
-document.getElementById('playPause').style.cursor = "pointer";
-document.getElementById('playPause').onclick = function() {
-	if (this.attributes.playing == true) {
-		this.attributes.playing = false;
-		this.src = 'pics/play.png';
-	} else {
-		this.attributes.playing = true;
-		this.src = 'pics/stop.png';
+function() {
+	var url = location.href;
+	url = url.split('/');
+	for (i = 0; i < url.length; i++) {
+		if (url[i] == 'beats.html') {
+			var run = true;
+		}
+	}
+	if (run === true) {
+		document.getElementById('playPause').style.cursor = "pointer";
+		document.getElementById('playPause').onclick = function() {
+			if (this.attributes.playing == true) {
+				this.attributes.playing = false;
+				this.src = 'pics/play.png';
+			} else {
+				this.attributes.playing = true;
+				this.src = 'pics/stop.png';
+			}
+		}
 	}
 }
